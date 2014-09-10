55c81901a3e3b1d6d77a9432bb303e0f66cd2e38
Richard Feng <richardfeng2@outlook.com>
2014-09-11 08:40:31 +1000

    Working login, gui looks crap though

--- src/medicalapp/guilogin/GuiLogin.java
+++ src/medicalapp/guilogin/GuiLogin.java
@@ -8,9 +8,8 @@ package medicalapp.guilogin;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -19,55 +18,14 @@ import javafx.stage.Stage;
  */
 public class GuiLogin extends Application {
 
-    private Stage primaryStage;
-    private BorderPane guiLogin;
-
     @Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("AddressApp");
-
-        initRootLayout();
-
-        showPersonOverview();
-    }
-
-    /**
-     * Initializes the root layout.
-     */
-    public void initRootLayout() {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(GuiLogin.class.getResource("guilogin/GuiLogin.fxml"));
-        Scene scene = new Scene(guiLogin);
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("GuiLogin.fxml"));
+        primaryStage.setTitle("MedicalApp");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
-    /**
-     * Shows the person overview inside the root layout.
-     */
-    public void showPersonOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(GuiLogin.class.getResource("view/PersonOverview.fxml"));
-            AnchorPane personOverview = (AnchorPane) loader.load();
-
-            // Set person overview into the center of root layout.
-            //guiLogin.setCenter(personOverview);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Returns the main stage.
-     *
-     * @return
-     */
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
     public static void main(String[] args) {
         launch(args);
     }
--- src/medicalapp/guilogin/GuiLoginController.java
+++ src/medicalapp/guilogin/GuiLoginController.java
@@ -3,39 +3,74 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package medicalapp.guilogin;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import medicalapp.data.DBConnection;
 
 /**
  *
  * @author Tony
  */
 public class GuiLoginController implements Initializable {
-    
+
     @FXML
-    private TextField username;
+    private TextField usernameField;
     @FXML
-    private PasswordField password;
+    private PasswordField passwordField;
+    @FXML
+    private Button loginButton;
     @FXML
     private Label message;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
-    @FXML
-    private void loginAction(ActionEvent event){
-        
     }
-    
+
+    @FXML
+    private void loginAction(ActionEvent event) {
+        Connection conn = DBConnection.getInstance().getConnection();
+        try {
+            String usernameText = usernameField.getText();
+            String passwordText = passwordField.getText();
+            String passwordDB = "";
+            String firstName = "";
+
+            String query = "SELECT password, firstName FROM Staff "
+                    + " INNER JOIN Person ON Staff.personID = Person.personID"
+                    + " WHERE username = ?";
+            PreparedStatement stm = conn.prepareStatement(query);
+            stm.setString(1, usernameText);
+
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                passwordDB = rs.getString("password");
+                firstName = rs.getString("firstName");
+            }
+            if (passwordDB.equals(passwordText) && !passwordText.equals("")) {
+                message.setText("\tWelcome " + firstName + " !");
+            }else {
+                message.setText("Invalid login details.");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GuiLoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
